8a94b3f33594d43ad99a287125204a408d4dc822
EliasEriksson <EliasEriksson95@gmail.com>
2021-10-22 16:00:46 +0200

    added browserSync, css minification and javascript minification. added run in development section in readme

--- README.md
+++ README.md
@@ -6,3 +6,9 @@ https://miun.eliaseriksson.eu/web3/moment6/client/
 ## Related projects
 * https://github.com/EliasEriksson/miun-web3-mom6-rest
 * https://github.com/EliasEriksson/miun-web3-mom6-admin
+
+
+## Run in development
+1. `npm install`
+2. If you do not have `gulp-cli` installed install with `npm install -g gulp-cli`
+3. `gulp`
--- gulpfile.js
+++ gulpfile.js
@@ -2,47 +2,71 @@ const {src, dest, watch, series, parallel} = require("gulp");
 const sass = require('gulp-sass')(require('sass'));
 const terser = require('gulp-terser');
 const cssnano = require("gulp-cssnano");
-const babel = require('gulp-babel');
 const ts = require('gulp-typescript');
-
-
+const browserSync = require("browser-sync").create();
 const tsProject = ts.createProject('tsconfig.json');
 
+// source roots
 const srcRoot = "./src";
 const srcPaths = {
     sass: `${srcRoot}/sass/**/*.scss`,
     ts: `${srcRoot}/ts/**/*.ts`,
 }
 
+// public roots
 const destRoot = "./public";
 const destPaths = {
     js: `${destRoot}/js/`,
     css: `${destRoot}/css`
 }
 
+/**
+ * compiles sass to css and minifies the css
+ *
+ * @returns {Stream.Transform|*}
+ */
 const compileSass = () => {
     return src(
         srcPaths.sass
     ).pipe(
         sass().on("error", sass.logError)
     ).pipe(
+        cssnano()
+    ).pipe(
         dest(destPaths.css)
+    ).pipe(
+        browserSync.stream()
     );
 }
 
+/**
+ * compiles TS to JS and minimizes the JS
+ *
+ * @returns {Stream.Transform|*}
+ */
 const compileTs = () => {
     return src(
         srcPaths.ts
     ).pipe(
         tsProject()
     ).pipe(
+        terser()
+    ).pipe(
         dest(destPaths.js)
+    ).pipe(
+        browserSync.stream()
     )
 }
 
+/**
+ * watch tasks
+ */
 const serve = () => {
-    watch(srcPaths.ts, compileTs);
-    watch(srcPaths.sass, compileSass);
+    browserSync.init({
+        server: "./public/"
+    })
+    watch(srcPaths.ts, compileTs).on("change", browserSync.reload);
+    watch(srcPaths.sass, compileSass).on("change", browserSync.reload);
 }
 
 exports.default = series(
